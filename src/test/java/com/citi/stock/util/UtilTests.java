c47112085432464510b7fc3e8a706a2f4ad1ceb3
薄荷味的 <jgqluc@126.com>
2023-03-05 21:23:38 +0800

    call python scripts

---
+++ src/test/java/com/citi/stock/util/UtilTests.java
@@ -0,0 +1,31 @@
+package com.citi.stock.util;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import org.python.util.PythonInterpreter;
+
+//@SpringBootTest
+public class UtilTests {
+    public static void main(String[] args) {
+        Process process;
+        try {
+            process = Runtime.getRuntime().exec(
+                    "python D:\\Javaaa\\stock\\src\\main\\java\\com\\citi\\stock\\util\\Runtime.py");
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            in.close();
+            process.waitFor();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
