7d2ff1018d07c1077f1e577a803f6984e03f8cc8
薄荷味的 <jgqluc@126.com>
2023-03-07 21:24:27 +0800

    user login with token

--- src/main/java/com/citi/stock/service/IUserFavoritesRelationService.java
+++ src/main/java/com/citi/stock/service/IUserFavoritesRelationService.java
@@ -1,5 +1,14 @@
 package com.citi.stock.service;
 
+import com.citi.stock.entity.UserFavoritesRelation;
+
+import java.util.List;
+
 public interface IUserFavoritesRelationService {
+    Integer addOneFavorite(UserFavoritesRelation userFavoritesRelation);
+
+    List<UserFavoritesRelation> getFavoritesByUid(Integer uid);
+
+    Integer deleteByUidAndCode(UserFavoritesRelation record);
 
 }
--- src/main/java/com/citi/stock/service/impl/UserFavoritesServiceImpl.java
+++ src/main/java/com/citi/stock/service/impl/UserFavoritesServiceImpl.java
@@ -1,4 +1,30 @@
 package com.citi.stock.service.impl;
 
-public class UserFavoritesServiceImpl {
+import com.citi.stock.entity.UserFavoritesRelation;
+import com.citi.stock.mapper.UserFavoritesRelationMapper;
+import com.citi.stock.service.IUserFavoritesRelationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserFavoritesServiceImpl implements IUserFavoritesRelationService {
+    @Autowired
+    private UserFavoritesRelationMapper userFavoritesRelationMapper;
+
+    @Override
+    public Integer addOneFavorite(UserFavoritesRelation userFavoritesRelation) {
+        return null;
+    }
+
+    @Override
+    public List<UserFavoritesRelation> getFavoritesByUid(Integer uid) {
+        return null;
+    }
+
+    @Override
+    public Integer deleteByUidAndCode(UserFavoritesRelation record) {
+        return null;
+    }
 }
